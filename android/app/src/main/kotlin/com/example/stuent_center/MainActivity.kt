b445b6ce33f4a1b52d8fe750aa7c2efd040cc636
Dasun Charuka <140306227+charukad@users.noreply.github.com>
2024-03-21 12:35:08 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/stuent_center/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stuent_center
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'student center',
+        home: Scaffold(
+          body: Text('student senter'),
+        ));
+  }
+}
